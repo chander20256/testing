d38a39ae31857ae6322cfa33c3cbe0c97d4bcba4
chander3012 <chanderprakash3012@gmail.com>
2026-05-02 19:44:10 +0530

    Fixed Render SPA routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import ShortlinksPage from "./pages/ShortlinksPage";
 
+
 export default function App() {
   return (
     <div className="min-h-screen bg-black text-white">
