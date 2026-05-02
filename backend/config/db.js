24a806dd6e329f3b92d74bec192fe3fea472fa14
chander3012 <chanderprakash3012@gmail.com>
2026-05-02 21:15:32 +0530

    Fixed mongodb production connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,38 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("MongoDB Connected");
+  try {
+
+    mongoose.set(
+      "strictQuery",
+      false
+    );
+
+    const conn =
+      await mongoose.connect(
+        process.env.MONGO_URI,
+        {
+          serverSelectionTimeoutMS: 10000,
+        }
+      );
+
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`
+    );
+
   } catch (error) {
-    console.error("MongoDB Connection Error:", error);
+
+    console.log(
+      "MongoDB Connection Error:"
+    );
+
+    console.log(error);
+
     process.exit(1);
+
   }
+
 };
 
 export default connectDB;
